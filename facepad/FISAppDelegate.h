18b4cb7247236438f0d3b6c4cdc5b4be4a0b4f73
Joe Burgess <joe@flatironschool.com>
2014-10-16 20:26:21 -0400

    Initial Commit

---
+++ facepad/FISAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  FISAppDelegate.h
+//  facepad
+//
+//  Created by iOS Staff on 10/16/14
+//  Copyright (c) 2014 The Flatiron School. All rights reserved.
+//
+
+@interface FISAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
